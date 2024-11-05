7cc4b4ab90e8b20a08af03d3caf30abdd8e0c3eb
swati <swati@Swati>
2024-11-05 08:58:58 +0530

    second push

---
+++ DemoFramework2/src/test/java/firstprogram/flipkartlogout.java
@@ -0,0 +1,11 @@
+package firstprogram;
+
+public class flipkartlogout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("flipkart logout successfully");
+
+	}
+
+}
